33f40374519272a0e0bbac27dbf2ba38d187d472
rikeshhh <70895896+rikeshhh@users.noreply.github.com>
2021-06-28 14:12:38 +0545

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,43 @@
+/*var newe = document.getElementById('new').value;
+var olde=document.getElementById('old').value;
+var anciente=document.getElementById('ancient').value;*/
+
+var i=0;
+var audio=new Audio("monkey.wav");
+var audio1=new Audio("cry.wav");
+function newe(){
+    audio1.play();
+    window.alert("wrong input try again!");
+}
+function old(){
+    audio.play();
+    window.alert('Your Score is'+i+1);
+}
+function ancient(){
+    audio1.play();
+    window.alert("wrong input try again!");
+}
+function newer(){
+    audio1.play();
+    window.alert("wrong input try again!");
+}
+function olde(){
+    audio1.play();
+    window.alert("wrong input try again!");
+}
+function ancienter(){
+    audio.play();
+    window.alert('Your Score is'+ 2);
+}
+function newers(){
+    audio.play();
+    window.alert('Your Score is'+ 3);
+}
+function olders(){
+    audio1.play();
+    window.alert("wrong input try again!");
+}
+function ancienters(){
+    audio1.play();
+    window.alert("wrong input try again!");
+}
